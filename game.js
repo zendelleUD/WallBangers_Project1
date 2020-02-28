ba4491877bcb9c8ce64fe80afdd4ace1784e49f9
Matthew Grossman <mattig@udel.edu>
2020-02-28 13:35:16 -0500

    Added initialize and reset function

--- game.js
+++ game.js
@@ -4,9 +4,17 @@ var wallbangers=function(){
         height:500,
         width:680,
         wallHeight:500,
-        wallWidth:340,
-        obstacleSpeed:50
+        wallWidth:340, // idea here is to have the wall width be half the width of the environment
+        obstacleSpeed:50,
+        minX:50,
+        minY:100
     }
-    this.score=0;
-    this.player = 
+    this.score=0; // This is the total number of points the player has accumulated so far
+    this.ninja = new player(); // Add the coordinates for the ninja. The goal is to have him on the bottom of the right wall essentially
+    this.initialize=function(){
+        self.reset();
+    };
+    this.reset=function(){
+        this.score = 0;
+    }
 }
