7f2c6f9ef05b506e216121a1e73e8efa2b03ec7c
Matthew Grossman <mattig@udel.edu>
2020-03-04 13:30:35 -0500

    Removed player function from Wallbangers function

--- scripts/WallBangers.js
+++ scripts/WallBangers.js
@@ -20,59 +20,55 @@ var wallBangers=function(){
     this.reset=function(){
         this.score = 0;
     }
+}
 
-
-    var player = function(xPos, yPos, minX, maxX, minY, maxY, veloX, veloY){
-        var self = this;
-        this.jump = false;
-        this.LtoR = true;
-        this.xPos = xPos;
-        this.yPos = yPos;
-        this.minX = minX;
-        this.maxX = maxX;
-        this.minY = minY;
-        this.maxY = maxY;
-        this.veloX = veloX;
-        this.veloY = veloY;
-        this.initialize = function(){};
-        this.setPosition = function(xPos, yPos){ // this is how we move the ninja
-            if(xPos < self.minX){
-                self.xPos = self.minX;
-            }
-            else if(xPos > self.maxX){
-                self.xPos = self.maxX;
-            }
-            if(yPos < self.minY){
-                self.yPos = self.minY;
-            }
-            if(yPos > self.maxY){
-                self.yPos = self.maxY;
-            }
-        };
-        /*this.incrementPosition = function(amount){
-            self.setPosition(self.xPos + amount, self.yPos + amount);
-        };*/
-        this.jump = function(){
-            
-            if(!jump){ //Check if already jumping
-                switch (this.LtoR) {
-                    case true:
-                        //this.veloX += 20; //Left to Right   
-                        self.setPosition(xPos + 20, yPos);                
-                        break;
-                    case false:
-                        //this.veloX -= 20; //Right to Left
-                        self.setPosition(xPos - 20, yPos);
-                        break;
-                }
-                this.jump = true;
-            }
-            this.veloY += 15; //Gravity
-            
-
-        };
-    }
-    this.generateHoles = function(){
+var player = function(xPos, yPos, minX, maxX, minY, maxY, veloX, veloY){
+    var self = this;
+    this.jump = false;
+    this.LtoR = true;
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.minX = minX;
+    this.maxX = maxX;
+    this.minY = minY;
+    this.maxY = maxY;
+    this.veloX = veloX;
+    this.veloY = veloY;
+    this.initialize = function(){};
+    this.setPosition = function(xPos, yPos){ // this is how we move the ninja
+        if(xPos < self.minX){
+            self.xPos = self.minX;
+        }
+        else if(xPos > self.maxX){
+            self.xPos = self.maxX;
+        }
+        if(yPos < self.minY){
+            self.yPos = self.minY;
+        }
+        if(yPos > self.maxY){
+            self.yPos = self.maxY;
+        }
+    };
+    /*this.incrementPosition = function(amount){
+        self.setPosition(self.xPos + amount, self.yPos + amount);
+    };*/
+    this.jump = function(){
         
-    }
+        if(!jump){ //Check if already jumping
+            switch (this.LtoR) {
+                case true:
+                    //this.veloX += 20; //Left to Right   
+                    self.setPosition(xPos + 20, yPos);                
+                    break;
+                case false:
+                    //this.veloX -= 20; //Right to Left
+                    self.setPosition(xPos - 20, yPos);
+                    break;
+            }
+            this.jump = true;
+        }
+        this.veloY += 15; //Gravity
+        
+
+    };
 }
