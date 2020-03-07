1905243b31dd57b66967e3b3d76a7b9ede9bd110
Zendelle Badiang <Zendelle@udel.edu>
2020-03-07 18:23:37 -0500

    Logic Checking collision with wall, if currently jumping do not add x velocity, and increase score every tick to simulate 'distance'

--- scripts/WallBangers.js
+++ scripts/WallBangers.js
@@ -4,14 +4,13 @@ var wallBangers=function(){
         height:500,
         width:680,
         wallHeight:500,
-        wallWidth:50, // idea here is to have the wall width be half the width of the environment
+        wallWidth:50,// idea here is to have the wall width be half the width of the environment
         obstacleSpeed:50,
         minX:50,
         minY:100
     }
 
-    
-    this.score=0; // This is the total number of points the player has accumulated so far
+    this.score = 0;   // This is the total number of points the player has accumulated so far
                     //player(xPos, yPos, minX, maxX, minY, maxY, veloX, veloY)
     this.ninja = new player(); // Add the coordinates for the ninja. The goal is to have him on the bottom of the right wall essentially
     this.initialize=function(){
@@ -20,22 +19,22 @@ var wallBangers=function(){
     this.reset=function(){
         this.score = 0;
     }
-
-
-    
+    this.updateScore = function(){
+        this.score +=1 ;
+    }
 
     this.update=function(){
-        
-        self.ninja.xPos += 1; 
+        self.ninja.xPos += self.ninja.veloX;
+        this.updateScore(); 
         // document.write("updating\n");
-        
+        self.ninja.isCollide();
     };
 
 }
 
 function player(){
     var self = this;
-    this.jump = false;
+    this.jumping = false;
     this.LtoR = false;
     this.xPos = 0;
     this.yPos = 0;
@@ -48,21 +47,38 @@ function player(){
     this.initialize = function(){};
     this.jump = function(){
         
-        if(!this.jump){ //Check if already jumping
-            switch (this.LtoR) {
-                case true:
-                    this.posX += 400; //Left to Right
-                    this.jump = false;                   
-                    break;
-                case false:
-                    this.posX += (this.posX + 100); //Right to Left
-                    this.jump = false;
-                    break;
-            }
-            this.jump = true;
+        // var currentlyJumping = this.jumping;
+        if(this.jumping == false && (this.xPos ==0 || this.xPos == 450)){
+            if(this.LtoR == true){
+                    this.veloX -= 4;
+                    this.jumping = true;
+                    console.log("Jumping right")
+                }else{
+                    this.veloX += 4;  
+                    this.jumping = true;
+                    console.log("Jumping left");
+                }
         }
-        this.veloY += 15; //Gravity
+       
+        console.log(this.veloX);
+    }
+    // this.xPos += this.veloX; //Gravity
+    console.log("This is xPos: " + this.xPos + "\nThis is veloX: " + this.veloX );
+    
+    this.isCollide=function(/*add possible parameters*/){
+       
+        this.veloX = (this.xPos > 450 || this.xPos < 0) ? 0: this.veloX;
         
+        this.jumping = (this.xPos > 450 || this.xPos < 0) ? false : this.jumping; 
 
+        this.xPos = (this.xPos > 450 ) ? 450 : this.xPos;
+        this.xPos =  (this.xPos < 0 ) ? 0 : this.xPos;
+
+        this.LtoR = (this.xPos == 450) ? true: this.LtoR;
+        this.LtoR = (this.xPos == 0) ? false: this.LtoR;
+        
     };
-}
+    
+    
+};
+
--- scripts/WallBangersUI.js
+++ scripts/WallBangersUI.js
@@ -7,18 +7,21 @@ var WallBangersUI=function()
     {
         //Initialize wallbangers.js
         //Back end
-        self.game=new wallBangers()
+        self.game=new wallBangers();
      
            $('#GameStopped').show();
             $('#GameRunning').hide();
      
+        
+        // new KeyboardEvent("onKeyPress",self.game.ninja.jump());
         // Key Event Listener
       $('body').keypress(function(event){
             if (event.which==32) //Keycode for space is 32
             {
-                self.game.ninja.jump();   
+                self.game.ninja.jump();
+                console.log("jumping");   
             }
-            $('#player').css("right",self.game.ninja.xPos+'px');
+            // $('#player').css("right",self.game.ninja.xPos+'px');
         });
 
 
@@ -37,17 +40,18 @@ var WallBangersUI=function()
             self.refreshView();
         });
     };
-    // this.refreshView=function(){
-    //     $('#player').css("right",self.game.ball.xPos + 'px');
+
+    this.refreshView=function(){
+        $('#player').css("right",self.game.ninja.xPos + 'px');
+        $('#player').css("bottom",self.game.ninja.yPos + 'px');
+        document.getElementById("score").innerText = self.game.score;
+    };
         
-    // };
-    
-    
     this.updateUI=function(){
         
             var result=self.game.update();
             // self.refreshView();
-            $('#player').css("right",self.game.ninja.xPos +'px');
+            self.refreshView();
             // this.game.update();
     }
     this.initialize();
